6e65b8df3a7500f8d04aa0d9e8e332d2dd5b737a
DarkDolphin1 <shravanrajapure1@gmail.com>
2026-02-02 05:00:32 +0530

    compresso : testing some basic operations

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include<iostream>
+#include<string>
+#include<fstream>
 
-  int main(void){
-    std::cout<<"\n Test file \n";
+  int main(int argc , char** argv){
+    
+    if(argc < 3){
+      std::cout<<"Not enough Arguments , exiting \n";
+      std::exit(1);
+    }
+
+    const std::string path(argv[1]);
+    std::string message(argv[2]);
+    std::cout<<"File path is : "<<path<<"\n";
+
+    std::ofstream outfile(path);
+    if(!outfile.is_open()){
+      std::cerr<<"Failed to open file: "<<path<<"\n";
+      std::cout<<"File may not exist , check the path provided \n";
+      std::exit(1);
+    }
+    outfile<<message;
+    outfile.close();
+
+    std::ifstream infile(path);
+    if(!infile.is_open()){
+      std::cout<<"Failed to read from the file: "<<path<<"\n";
+      std::exit(1);
+    }
+
+    std::string content;
+    infile>>content;
+    std::cout<<"\n Test file content :"<<content<<std::endl;
     return 0;
   }
 
