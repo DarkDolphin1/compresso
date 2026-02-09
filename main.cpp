8e6d476d19bb4ec2e9f979f482cc35c3b7ffdda3
DarkDolphin1 <shravanrajapure1@gmail.com>
2026-02-09 20:25:53 +0530

    add binary tree structure

--- main.cpp
+++ main.cpp
@@ -9,14 +9,14 @@
     std::cout<<" calling with insufficient paremeters will show this message \n ";
     std::cout<<" THIS IS A TEST BRANCH AND DOES NOT RELATE TO FINAL INTENDED BEHAVIOUR \n";
   }
+
   int main(int argc , char** argv){
-    
+
     if(argc < 3){
       std::cout<<"Not enough Arguments , see guide for more info \n";
       printUsage();
       std::exit(1);
     }
-
 
     const std::string path(argv[1]);
     std::string message(argv[2]);
@@ -42,4 +42,3 @@
     std::cout<<"\n Test file content :"<<content<<std::endl;
     return 0;
   }
-
--- src/readFile.cpp
+++ src/readFile.cpp
@@ -1,10 +1,18 @@
 #include "include/readFile.h"
+#include<cstdint>
 
-    void readFile(std::string result , std::ifstream &in){
+struct Node {
+    uint8_t data;
+    int freq;
+    Node *left, *right;
+    Node(uint8_t d, int f) : data(d), freq(f), left(nullptr), right(nullptr) {}
+};
+
+void readFile(std::string result , std::ifstream &in){
         std::string buffer("");
         
         if(!in.is_open()){
-          std::cout<<"[ERROR] readFile: can not access input stream , is the file path valid ?"
+          std::cout<<"[ERROR] readFile: can not access input stream , is the file path valid ?";
           return;
         }
 
