2fef19aeb0ca997f0eb2b2bfddbc8fc0f6e96838
DarkDolphin1 <shravanrajapure1@gmail.com>
2026-02-11 08:56:13 +0530

    allow generating custom entropy data

--- test/test.cpp
+++ test/test.cpp
@@ -23,14 +23,16 @@
         // generally the lifetime of the object should be determined by the scope where it is created rather than a helper function 
     }
 
-    void write_random(std::ofstream outstream , const size_t WRITE_LIMIT = 500ULL * 1024 * 1024 , const size_t CHUNK_SIZE = 1ULL * 1024 * 1024 ){
+    void write_random(std::ofstream &outstream , const size_t WRITE_LIMIT = 500ULL * 1024 * 1024 , const size_t CHUNK_SIZE = 1ULL * 1024 * 1024 ,
+                      uint8_t start = 0 , uint8_t end = 256  ){
+
         if(!outstream.is_open()){
             std::cout<<"Error : failed to write test cases \n";
             return;
         }
 
         std::mt19937 rng(std::random_device{}());
-        std::uniform_int_distribution<uint8_t> dist(0, 255); // Generate random data , in theory this should be harder to compress as we can't really find a strong pattern here
+        std::uniform_int_distribution<uint8_t> dist(start, end-1); // Generate random data , in theory this should be harder to compress as we can't really find a strong pattern here
 
         std::vector<uint8_t> buffer(CHUNK_SIZE);
 
