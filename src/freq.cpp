06a9f2aa283ba82cd7d543870a057284df059b3a
DarkDolphin1 <shravanrajapure1@gmail.com>
2026-02-17 12:09:00 +0530

    freq : seperate heap related code from freq

--- src/freq.cpp
+++ src/freq.cpp
@@ -28,14 +28,3 @@
 
     }
 
-std::priority_queue<Node*, std::vector<Node*>, Compare> minHeap; // will be using pointers to node instead of actual nodes 
-
-    void buildHeap(){
-        for(int i = 0; i < 256; i++){
-            if(freq[i] > 0){
-                Node *node = new Node { freq[i] , static_cast<uint8_t>(i) , nullptr , nullptr };
-
-                minHeap.push(node);
-            }
-        }
-    }
