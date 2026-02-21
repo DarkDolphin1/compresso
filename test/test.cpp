5a6f46070c16bcb78e080a8b8d92286381f0065a
DarkDolphin1 <shravanrajapure1@gmail.com>
2026-02-21 22:49:01 +0530

    make test unit accept arguments during runtime

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,10 @@
 // TODO : use multi-threading to speed things up
 // TODO : improve memory usage
+// TODO : add input validation to argv values passed , if invalid default to 0-255
 #include"test.h"
 #include<cstdint> 
 #include<random>
-
+#include<string>
 
     void write_linear(std::ofstream &outstream , const size_t WRITE_LIMIT = 500ULL * 1024 * 1024 , const size_t CHUNK_SIZE = 1ULL * 1024 * 1024 ){
         if(!outstream.is_open()){
@@ -23,8 +24,8 @@
         // generally the lifetime of the object should be determined by the scope where it is created rather than a helper function 
     }
 
-    void write_random(std::ofstream &outstream , const size_t WRITE_LIMIT = 500ULL * 1024 * 1024 , const size_t CHUNK_SIZE = 1ULL * 1024 * 1024 ,
-                      uint8_t start = 0 , uint8_t end = 255  ){
+    void write_random(std::ofstream &outstream , uint8_t start = 0 , uint8_t end = 255 ,
+                      const size_t WRITE_LIMIT = 500ULL * 1024 * 1024 , const size_t CHUNK_SIZE = 1ULL * 1024 * 1024){
 
         if(!outstream.is_open()){
             std::cout<<"Error : failed to write test cases \n";
@@ -47,14 +48,21 @@
         }
     }
 
-    int main (){
+    int main (int argc , char** argv){
         std::ofstream outLinear("linear.bin",std::ios::binary);
         std::ofstream outRandom("random.bin",std::ios::binary);
 
+        size_t start = 0;
+        size_t end = 255;
+
+        if(argc == 3){
+            start = stoi(std::string(argv[1])); // this does not deal with invalid inputs btw
+            end = stoi(std::string(argv[2]));  // same thing , we intend to mainly use this with a script so not the first priority
+        }
         std::cout<<"generating linear data ....\n";
         write_linear(outLinear);
         std::cout<<"generating random data ....\n";
-        write_random(outRandom);
+        write_random(outRandom,start,end);
         std::cout<<" Test case generated ! \n";
         return 0;
     }
