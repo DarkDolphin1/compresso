f5fcaf113d0213886ce2eaa0315876f64a34f8f5
DarkDolphin1 <shravanrajapure1@gmail.com>
2026-02-21 23:21:06 +0530

    take input filename as an argument

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<string>
 #include<fstream>
+#include<filesystem>
 #include"compresso.h"
 
   void printUsage(){
@@ -11,15 +12,21 @@
   }
 
   int main(int argc , char** argv){
+    std::string output = "comp.bin";
+    std::string input;
+    if(argc > 1){
+    	input = argv[1];
+        if(!std::filesystem::exists(input)){
+			std::cout<<"Given file does not exist , check the file name and try again \n";
+			std::exit(1);
+        }
+    } else {
+		std::cout<<"Provide a file to compress \n";
+		printUsage();
+		std::exit(1);
+	}
 
-    if(argc < 3){
-      std::cout<<"Not enough Arguments , see guide for more info \n";
-      printUsage();
-      std::exit(1);
-    }
-
-    // hard coding file names for now
-    compresso("random.bin" , "comp_random.bin");
+    compresso(input,output);
 
     return 0;
   }
